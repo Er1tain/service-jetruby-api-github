c0a673a482375c329c56c220f89f5151a28bb779
unknown <sanyapridava@mail.ru>
2024-07-19 00:06:56 +0300

    Create handler what get data about repo by means his id

--- api/server.js
+++ api/server.js
@@ -1,4 +1,20 @@
+const { default: axios } = require('axios');
 const express = require('express');
 const app = express();
+
+//Эндпоинт для получения информации о репозитории по имени или id
+app.get('/repo', async (request, response)=>{
+    try {
+        const repo_id = request.query.id;
+        const query = await axios.get(`https://api.github.com/repositories/${repo_id}`);
+        const data = await query.data;
+        
+        response.json(data);
+    } catch(error) {
+        response.status(400).json({
+            result: "Repo not found..("
+        })
+    }
+})
 
 app.listen(3001, ()=>console.log('Server run!'));
