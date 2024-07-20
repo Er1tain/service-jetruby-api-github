32879e8216bc56cd5a4671f52aa0e66c6a36659d
unknown <sanyapridava@mail.ru>
2024-07-20 22:04:03 +0300

    Class for work with database

---
+++ api/database.js
@@ -0,0 +1,41 @@
+//Симмуляция работы с БД
+class DataBase {
+    //Создание объекта для установки соединения с БД и начала работы с ней
+    constructor(){
+
+    }
+    
+    //Добавление в БД одного репозитория
+    add_one_repo(repo) {
+        if (typeof(repo) === 'object' && repo.length === undefined) {
+
+        }
+    }
+
+    //Добавление множества репозиториев
+    add_many_repo(repos) {
+        if (typeof(repo) === 'object' && repo.length > 0) {
+
+        }
+    }
+
+    update_repo(repo_id) {
+        if (typeof(repo_id) === 'string') {
+
+        }
+    }
+
+    delete_repo() {
+        if (typeof(repo_id) === 'string') {
+
+        }
+    }
+
+    get_repo() {
+        if (typeof(repo_id) === 'string') {
+
+        }
+    }
+
+
+}
