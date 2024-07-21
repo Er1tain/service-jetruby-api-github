112bf00ac1fec1c84b3c76c73b0fcbd71e107c08
unknown <sanyapridava@mail.ru>
2024-07-21 12:15:21 +0300

    Create panel button

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './styles/App/h1.css';
 import './styles/App/header.css';
-import './styles/App/logo.css'
+import './styles/App/logo.css';
+import './styles/App/button_panel.css';
+import './styles/App.css'
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
           <div id="logo_github"></div>
           <h1>API GitHub</h1>
         </header>
+        <div id="button_panel">
+          <button class="button_panel_button" id="popular_repos">Данные о  <br></br>популярных репозиториях</button>
+          <button class="button_panel_button" id="get_repo_id">Получить информацию <br></br>о репозитории по id</button>
+          <button class="button_panel_button" id="get_repo_owner_reponame">Получить информацию <br></br>о репозитории по нику владельца и названию</button>
+        </div>
     </div>
   );
 }
