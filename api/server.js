5516ff9d934e763ebc01cfd9b0814e90ef3fb127
unknown <sanyapridava@mail.ru>
2024-07-21 14:46:50 +0300

    settings CORS

--- api/server.js
+++ api/server.js
@@ -1,7 +1,24 @@
 import axios from 'axios';
 import express from 'express';
 import DataBase from './database.js';
+import cors from 'cors';
+
 const app = express();
+
+//Настроить политику CORS
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  };
+app.use(cors(corsOptions));
+//
 
 //Объект для работы с БД
 const DB = new DataBase();
