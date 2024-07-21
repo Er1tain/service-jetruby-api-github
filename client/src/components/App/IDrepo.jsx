0cef05dc69fa8e59b0bf6d8efb15f332ef95c254
unknown <sanyapridava@mail.ru>
2024-07-22 02:34:43 +0300

    Create modal window with info about repo

--- client/src/components/App/IDrepo.jsx
+++ client/src/components/App/IDrepo.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useRef, useState } from 'react';
+import '../../styles/IDrepo.css';
+
 export default function IDrepo() {
+    const [visible, Visible] = useState(false);
+
+    const id_input = useRef();
+
+    useEffect(()=>{
+        if (visible) {
+            
+        }
+    }, [visible])
+
     return (
         <div id="IDrepo">
-            
+            {
+                visible && (
+                    <div id="container">
+                        <div id="content">
+                            <div id="close_btn" onClick={()=>Visible(false)}>
+                                <div id="image_crest"></div>
+                            </div>
+                            <div id="characters">
+                                <div id="owner">
+                                    <label>Владелец: </label>
+                                </div>
+                                <div id="repo_name">
+                                    <label>Имя репозитория: </label>
+                                </div>
+                                <div id="link">
+                                    <label>Ссылка: </label>
+                                </div>
+                                <div id="stars">
+                                    <labe>Кол-во звёзд: </labe>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                )
+            }
+            <h1>Репозиторий по ID</h1>
+            <form>
+                <div>
+                    <label>ID репозитория</label>
+                    <input placeholder='ID' ref={id_input}/>
+                </div>
+                <button onClick={(e)=>{
+                    e.preventDefault();
+                    if (id_input.current.value != '') Visible(true)    
+                }}>Информация</button>
+            </form>
         </div>
     )
 }
