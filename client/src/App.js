1ccec1d4fff60434e6464a87e2d61dec49a45ec1
unknown <sanyapridava@mail.ru>
2024-07-21 13:14:23 +0300

    Add animation for logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,23 @@ import './styles/App/h1.css';
 import './styles/App/header.css';
 import './styles/App/logo.css';
 import './styles/App/button_panel.css';
-import './styles/App.css'
+import './styles/App.css';
+import './styles/App/header/animation.css';
 import PanelButton from './components/App/PanelButton';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const logo = useRef();
+
+  useEffect(()=>{
+    logo.current.style.transform = '2s';
+    logo.current.style.rotate = '360deg';
+  }, [])
+
   return (
     <div>
         <header>
-          <div id="logo_github"></div>
+          <div id="logo_github" ref={logo}></div>
           <h1>API GitHub</h1>
         </header>
         <PanelButton/>
