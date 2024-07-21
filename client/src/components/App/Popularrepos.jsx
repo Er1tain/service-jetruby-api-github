768a31758b03f38289c9a4ca3c78c586a00f17c7
unknown <sanyapridava@mail.ru>
2024-07-21 14:21:43 +0300

    Page with top popular repos

--- client/src/components/App/Popularrepos.jsx
+++ client/src/components/App/Popularrepos.jsx
@@ -1,7 +1,18 @@
+import '../../styles/PopularRepos.css';
+import '../../styles/PopularRepos/h1.css';
+import '../../styles/PopularRepos/Most_popular_buttons.css';
+
 export default function PopularRepos() {
     return (
         <div id="Most_popular_repos">
-            
+            <h1>Самые популярные репозитории</h1>
+            <div id="top-repos">
+                
+            </div>
+            <div id="Most_popular_buttons">
+                <button id="Back_to_main">На главную</button>
+                <button id="update_popular_repos">Обновить</button>
+            </div>
         </div>
     )
 }
