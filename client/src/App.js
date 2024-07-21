f76ec4228a5f130a9bec74bb8ed4bc183b29c158
unknown <sanyapridava@mail.ru>
2024-07-21 11:56:37 +0300

    Add styles in App.js and create simple structure component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
+import './styles/App/h1.css';
+import './styles/App/header.css';
+import './styles/App/logo.css'
+
 function App() {
   return (
     <div>
-        
+        <header>
+          <div id="logo_github"></div>
+          <h1>API GitHub</h1>
+        </header>
     </div>
   );
 }
