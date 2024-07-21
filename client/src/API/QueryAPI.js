aec0ac3c2cb1d7632c77a4379ce0f522685f8ef3
unknown <sanyapridava@mail.ru>
2024-07-21 13:35:52 +0300

    Request to back-end

---
+++ client/src/API/QueryAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export default class QueryAPI {
+    static get_popular_repos() {
+        
+    }
+
+    static get_repo_id() {
+
+    }
+
+    static get_repo_owner_reponame() {
+
+    }
+}
