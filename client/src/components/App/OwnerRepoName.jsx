5c01d0af32702395221098f600ccc15d094c140b
unknown <sanyapridava@mail.ru>
2024-07-22 21:05:12 +0300

    Create skelet page

--- client/src/components/App/OwnerRepoName.jsx
+++ client/src/components/App/OwnerRepoName.jsx
@@ -1,7 +1,19 @@
 export default function OwnerRepoName() {
     return (
         <div id="OwnerRepoName">
-
+            <h1>Репозиторий по нику и названию</h1>
+            <form>
+                <div>
+                    <label>Ник</label>
+                    <input placeholder="username"/>
+                </div>
+                <div>
+                    <label>Имя репозитория</label>
+                    <input placeholder="reponame"/>
+                </div>
+                <button>Информация</button>
+                <button>На главную</button>
+            </form>
         </div>
     )
 }
