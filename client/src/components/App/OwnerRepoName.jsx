1a14daa6ac67254c385d8ec744fd7f2ce8d454c3
unknown <sanyapridava@mail.ru>
2024-07-22 21:22:35 +0300

    Add styles

--- client/src/components/App/OwnerRepoName.jsx
+++ client/src/components/App/OwnerRepoName.jsx
@@ -1,4 +1,9 @@
+import { useNavigate } from 'react-router-dom';
+import '../../styles/OwnerRepoName.css';
+
 export default function OwnerRepoName() {
+    const navigate = useNavigate();
+
     return (
         <div id="OwnerRepoName">
             <h1>Репозиторий по нику и названию</h1>
@@ -12,7 +17,7 @@ export default function OwnerRepoName() {
                     <input placeholder="reponame"/>
                 </div>
                 <button>Информация</button>
-                <button>На главную</button>
+                <button id="back_btn" onClick={()=>navigate('/')}>На главную</button>
             </form>
         </div>
     )
